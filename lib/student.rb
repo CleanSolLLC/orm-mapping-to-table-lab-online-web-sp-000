ba954153f31c26d3bdf4263372c5f8cd4f63b2f0
Mark May <mamark90@gmail.com>
2020-07-20 21:28:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,7 +41,7 @@ class Student
     
     def self.create(student_info)
       student_info.each do |key, value|
-      self.send(("#{key}="), value)
+      Student.send(("#{key}="), value)
       binding.pry
       end
     end
