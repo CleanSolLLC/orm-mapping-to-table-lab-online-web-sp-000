d0d6a59de2af1a7a000ab7dacc041a55df275a15
Mark May <mamark90@gmail.com>
2020-07-20 21:14:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,17 +3,13 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  # def initialize(name, grade, id = nil)
-  #   @name = name
-  #   @grade = grade
-  #   @id = id
-  # end
-  
-  def initialize(attributes)
-    attributes.each do |key, value|
-    self.send("#{key}=", value)
-    end
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
   end
+  
+
     
   def self.create_table
       
